3b581b07b9d2336d114c342a81bd5362697b749c
intsuc <s@intsuc.dev>
2025-12-21 19:37:18 +0900

    Add int64

--- core/src/main/kotlin/Elaborate.kt
+++ core/src/main/kotlin/Elaborate.kt
@@ -11,6 +11,10 @@ sealed interface Abstract {
 
     data class Type(override val span: Span) : Abstract
 
+    data class Int64(override val span: Span) : Abstract
+
+    data class Int64Lit(val value: Long, override val span: Span) : Abstract
+
     data class Var(val text: String, val index: Index, override val span: Span) : Abstract
 
     data class Err(val message: String, override val span: Span) : Abstract
@@ -18,6 +22,10 @@ sealed interface Abstract {
 
 sealed interface Value {
     data object Type : Value
+
+    data object Int64 : Value
+
+    data class Int64Lit(val value: Long) : Value
 
     data class Var(val text: String, val level: Level) : Value
 
@@ -52,6 +60,8 @@ private fun ElaborateState.diagnose(message: String, span: Span): Abstract {
 private fun conv(term1: Value, term2: Value): Boolean {
     return when (term1) {
         is Value.Type if term2 is Value.Type -> true
+        is Value.Int64 if term2 is Value.Int64 -> true
+        is Value.Int64Lit if term2 is Value.Int64Lit -> term1.value == term2.value
         is Value.Var if term2 is Value.Var -> term1.level == term2.level
         else -> false
     }
@@ -61,16 +71,20 @@ private fun ElaborateState.synth(term: Concrete): SynthResult {
     return when (term) {
         is Concrete.Ident -> when (term.text) {
             "type" -> SynthResult(Abstract.Type(term.span), Value.Type)
+            "int64" -> SynthResult(Abstract.Int64(term.span), Value.Int64)
             else -> when (val level = entries.indexOfLast { it.name == term.text }) {
-                -1 -> {
-                    val message = "Unknown identifier: ${term.text}"
-                    SynthResult(diagnose(message, term.span), Value.Err(message))
+                -1 -> when (val value = term.text.toLongOrNull()) {
+                    null -> {
+                        val message = "Unknown identifier: ${term.text}"
+                        SynthResult(diagnose(message, term.span), Value.Err(message))
+                    }
+
+                    else -> SynthResult(Abstract.Int64Lit(value, term.span), Value.Int64Lit(value))
                 }
 
                 else -> {
                     val index = (entries.lastIndex - level).toUInt()
                     SynthResult(Abstract.Var(term.text, index, term.span), entries[level].type)
-
                 }
             }
         }
