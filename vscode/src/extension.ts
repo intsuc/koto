3fb7b467008fb3293ad132c7a261c3344a94e1ae
intsuc <intsuc@gmail.com>
2025-12-24 11:33:15 +0900

    Add feature to replace `->` with `→`

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -1,9 +1,43 @@
-import type { ExtensionContext } from "vscode";
+import { Range, WorkspaceEdit, workspace } from "vscode";
+import type { Disposable, ExtensionContext } from "vscode";
 import { LanguageClient } from "vscode-languageclient/node";
 
 let client: LanguageClient;
 
-export function activate(_context: ExtensionContext) {
+function arrowEdit(): Disposable {
+  let applying = false;
+  return workspace.onDidChangeTextDocument(async (event) => {
+    if (applying) return;
+    if (event.document.languageId !== "koto") return;
+    if (event.contentChanges.length !== 1) return;
+
+    const change = event.contentChanges[0]!;
+    if (change.text !== ">") return;
+    if (change.rangeLength !== 0) return;
+
+    const endOffset = change.rangeOffset + change.text.length;
+    const startOffset = endOffset - "->".length;
+    if (startOffset < 0) return;
+
+    const range = new Range(
+      event.document.positionAt(startOffset),
+      event.document.positionAt(endOffset),
+    );
+    const text = event.document.getText(range);
+    if (text !== "->") return;
+
+    applying = true;
+    try {
+      const edit = new WorkspaceEdit();
+      edit.replace(event.document.uri, range, "→");
+      await workspace.applyEdit(edit);
+    } finally {
+      applying = false;
+    }
+  })
+}
+
+export function activate(context: ExtensionContext) {
   client = new LanguageClient(
     "koto",
     "ヿ Language Server",
@@ -19,6 +53,10 @@ export function activate(_context: ExtensionContext) {
     },
   );
   client.start();
+
+  context.subscriptions.push(
+    arrowEdit(),
+  );
 }
 
 export function deactivate() {
