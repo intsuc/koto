cc19d20a22f71b2bd2604b7279241127fcada9bc
intsuc <s@intsuc.dev>
2026-01-02 06:47:59 +0900

    Add `build` subcommand

--- cli/src/main/kotlin/Main.kt
+++ cli/src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 package koto.cli
 
 import com.github.ajalt.clikt.core.*
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
 import koto.lsp.KotoLanguageServer
 
 class Koto : NoOpCliktCommand()
@@ -10,8 +12,17 @@ class Lsp : CliktCommand() {
     override fun run() = KotoLanguageServer.launch(System.`in`, System.out)
 }
 
+class Build : CliktCommand() {
+    val input: String by option("-i", "--input").required()
+
+    override fun run() {
+        echo("Building $input")
+    }
+}
+
 fun main(args: Array<String>) = Koto()
     .subcommands(
+        Build(),
         Lsp(),
     )
     .main(args)
